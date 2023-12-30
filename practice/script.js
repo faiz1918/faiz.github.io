046e53b23bd05d3ad12bff020cadb73f25bc8561
faiz <mohammadfaiz9940@gmail.com>
2023-12-30 19:09:28 +0530

    html css concepts in practice repo

---
+++ practice/script.js
@@ -0,0 +1,11 @@
+// function twoTable(inputValue) {
+//     return inputValue*2
+// }
+
+// var myFinalvalue = twoTable(3);
+// alert("Final Value is: " + myFinalvalue);
+
+// var myFinalvalue:Number;
+// twoTable(inputValue) {
+
+// }
